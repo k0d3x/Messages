dc7fec0b9156c726f7af133fc5af4d181f7adf5b
Ameer <aamirkhn89@gmail.com>
2019-01-14 22:35:50 +0530

    messaging_app_second_commit

--- app/src/main/java/com/ameer/messages/activities/ChatActivity.java
+++ app/src/main/java/com/ameer/messages/activities/ChatActivity.java
@@ -8,7 +8,6 @@ import android.content.pm.PackageManager;
 import android.database.Cursor;
 import android.net.Uri;
 import android.os.Bundle;
-import android.os.SystemClock;
 import android.provider.Telephony;
 import android.support.annotation.Nullable;
 import android.support.v4.app.ActivityCompat;
